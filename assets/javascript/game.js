58ecae614412c670a607ab664896f41cb7ae6a2f
neagtivefriction <chris.chrispete@gmail.com>
2019-03-03 01:34:02 -0600

    Coded the whole thing. Used jQuery. Hope that is okay.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,14 +9,62 @@ document.addEventListener("DOMContentLoaded", function() {
       gemArray[i] = Math.ceil(Math.random() * 11) + 1;
     }
   }
-  var targetV;
+  var targetVal;
 
   // Determine our target value as a random number between 19 and 139.
   //   I would love to make this as a function of the gems instead
   // of just a random number, but then there isn't a good way to ensure that it's
   // between 19 and 120 without writing a more complicated algorithm than I have
   // time for this week.
-  function targetVal() {
-    targetVal += Math.floor(Math.random() * 139) + 19;
+  function getTargetVal() {
+    targetVal = Math.floor(Math.random() * 139) + 19;
   }
+
+  function reset() {
+    totalScore = 0;
+    gemvals();
+    getTargetVal();
+    updateTargetVal();
+    updateWinsAndLosses();
+    updateScore();
+  }
+
+  function updateTargetVal() {
+    $("#random-number").text(targetVal);
+  }
+
+  function updateWinsAndLosses() {
+    $("#wins").text("Wins: " + wins);
+    $("#losses").text("Losses: " + losses);
+  }
+
+  function updateScore() {
+    console.log("Entered updateScore");
+    $("#total-score").text(totalScore);
+  }
+
+  //   Create a link back to the document. The gem class applies to each of the gem pictures.
+  var allGems = $(".gem");
+  var totalScore = 0;
+  var wins = 0;
+  var losses = 0;
+
+  reset();
+
+  allGems.on("click", function() {
+    var gemIndex = parseInt($(this).attr("value")) - 1;
+    totalScore += gemArray[gemIndex];
+    console.log(totalScore);
+    updateScore();
+
+    if (totalScore === targetVal) {
+      wins++;
+      reset();
+    }
+
+    if (totalScore > targetVal) {
+      losses++;
+      reset();
+    }
+  });
 });
